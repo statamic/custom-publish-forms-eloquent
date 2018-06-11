0fd5a87f4c59282d92e7f066f2b9fbb3b4524d8e
Jack McDade <jack@jackmcdade.com>
2018-06-11 14:20:13 -0400

    Style the Products index page

--- site/addons/Products/resources/views/index.blade.php
+++ site/addons/Products/resources/views/index.blade.php
@@ -2,10 +2,38 @@
 
 @section('content')
 
-    <a href="{{ route('products.create') }}">Create Product</a>
-
-    @foreach ($products as $product)
-        <li><a href="{{ route('products.edit', ['product' => $product->id]) }}">{{ $product->title }}</a></li>
-    @endforeach
+    <div class="flex items-center mb-3">
+        <h1 class="flex-1">Products</h1>
+        <a href="{{ route('products.create') }}" class="btn btn-primary">Create Product</a>
+    </div>
+    <div class="card flush dossier">
+        <div class="dossier-table-wrapper">
+            <table class="dossier">
+                <thead>
+                    <tr>
+                        <th class="column-title">Title</th>
+                        <th class="column-slug">Slug</th>
+                        <th class="column-date">Date</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($products as $product)
+                    <tr>
+                        <td class="cell-title first-cell">
+                            <span class="column-label">Title</span>
+                            <a class="" href="{{ route('products.edit', ['product' => $product->id]) }}">
+                                {{ $product->title }}
+                            </a>
+                        </td>
+                        <td class="cell-slug">
+                            <span>what-i-did-last-summer</span>
+                        </td>
+                        <td class="cell-date">{{ $product->created_at->format('Y/m/d')}}</td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
 
 @endsection
